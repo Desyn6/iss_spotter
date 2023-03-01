239a93a54d5dd253989ffae0753bdccab2c48897
Denny <Dennyng1989@hotmail.com>
2023-02-28 21:52:57 -0800

    Implement chained callback functions

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
-// index.js
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
+nextISSTimesForMyLocation((error, passbys) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-  console.log('It worked! Returned IP:' , ip);
-});
 
-// test ip address
-const ip = '64.180.195.1';
-fetchCoordsByIP(ip, (err, loc) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Success! Coordinates:`, loc);
+  for (const passby of passbys) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(passby.risetime);
+    const duration = passby.duration;
+    console.log(`Next passby at ${dateTime} for ${duration} seconds.`);
   }
 });
 
-// test fetch location
-const coords = { latitude: 49.2488091, longitude: -122.9805104 };
-fetchISSFlyOverTimes(coords, (err, passbys) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(passbys);
-  }
-});
+// old index.js code up to ISS IV
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// // test ip address
+// const ip = '64.180.195.1';
+// fetchCoordsByIP(ip, (err, loc) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(`Success! Coordinates:`, loc);
+//   }
+// });
+
+// // test fetch location
+// const coords = { latitude: 49.2488091, longitude: -122.9805104 };
+// fetchISSFlyOverTimes(coords, (err, passbys) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(passbys);
+//   }
+// });
